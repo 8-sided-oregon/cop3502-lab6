ec17a33567cb6f14b1a660ef69cb48a8f0e335af
8-sided-oregon <notebook23214@pm.me>
2024-03-20 21:17:41 -0400

    added checks to user input

--- encoder.py
+++ encoder.py
@@ -23,14 +23,22 @@ def main():
         
         if selected_option == 1:
             # User wants to encode a password
-            encoded = encode(input("Please enter your password to encode: "))
+            user_input = input("Please enter your password to encode: ")
+            # Repeatedly bugs the user until they enter a correct password
+            while len(user_input) != 8 or not user_input.isdigit():
+                print("Please input a valid 8 digit numeric password!")
+                user_input = input("Please enter your password to encode: ")
+            encoded = encode(user_input)
             print("Your password has been encoded and stored!")
         elif selected_option == 2:
+            # User wants to decode their previously entered password
             if encoded is not None:
                 decoded = decode(encoded)
                 print(f"The encoded password is: {encoded}, and the original password is: {decoded}.")
         elif selected_option == 3:
             return
+        else:
+            print("Please input a valid selection (1-3)")
 
 if __name__ == "__main__":
     main()
